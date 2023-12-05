6cbdf294b48ba94037b73cb46e99952c5b0f115a
mhmo91 <eng.mhmo91@gmail.com>
2023-12-05 13:00:52 +0200

    .

--- src/layout/flex/flex.ts
+++ src/layout/flex/flex.ts
@@ -6,60 +6,53 @@ import Layout from '../layout/layout'
 import style from './flex.scss?inline'
 @customElement('schmancy-flex')
 export class SchmancyFlex extends Layout {
-  static styles = [Layout.styles, unsafeCSS(style)]
-  layout = true
-  @property({ type: String, reflect: true }) flow: 'row' | 'row-reverse' | 'col' | 'col-reverse' =
-    'row'
-  @property({ type: String, reflect: true }) wrap: 'wrap' | 'nowrap' | 'wrap-reverse' = 'wrap'
-  @property({ type: String, reflect: true }) align:
-    | 'start'
-    | 'center'
-    | 'end'
-    | 'stretch'
-    | 'baseline' = 'start'
-  @property({ type: String, reflect: true }) justify: 'start' | 'center' | 'end' | 'stretch' =
-    'start'
-  @property({ type: String, reflect: true }) gap: string | undefined
+	static styles = [Layout.styles, unsafeCSS(style)]
+	layout = true
+	@property({ type: String, reflect: true }) flow: 'row' | 'row-reverse' | 'col' | 'col-reverse' = 'col'
+	@property({ type: String, reflect: true }) wrap: 'wrap' | 'nowrap' | 'wrap-reverse' = 'wrap'
+	@property({ type: String, reflect: true }) align: 'start' | 'center' | 'end' | 'stretch' | 'baseline' = 'start'
+	@property({ type: String, reflect: true }) justify: 'start' | 'center' | 'end' | 'stretch' = 'start'
+	@property({ type: String, reflect: true }) gap: string | undefined
 
-  render() {
-    const classes = {
-      flex: true,
-      // Direction
-      'flex-col': this.flow === 'row',
-      'flex-col-reverse': this.flow === 'row-reverse',
-      'flex-row': this.flow === 'col',
-      'flex-row-reverse': this.flow === 'col-reverse',
-      // Wrap
-      'flex-wrap': this.wrap === 'wrap',
-      'flex-wrap-reverse': this.wrap === 'wrap-reverse',
-      'flex-nowrap': this.wrap === 'nowrap',
-      // Justify
-      'items-start': this.justify === 'start',
-      'items-center': this.justify === 'center',
-      'items-end': this.justify === 'end',
-      'items-stretch': this.justify === 'stretch',
+	render() {
+		const classes = {
+			flex: true,
+			// Direction
+			'flex-col': this.flow === 'row',
+			'flex-col-reverse': this.flow === 'row-reverse',
+			'flex-row': this.flow === 'col',
+			'flex-row-reverse': this.flow === 'col-reverse',
+			// Wrap
+			'flex-wrap': this.wrap === 'wrap',
+			'flex-wrap-reverse': this.wrap === 'wrap-reverse',
+			'flex-nowrap': this.wrap === 'nowrap',
+			// Align
+			'items-start': this.align === 'start',
+			'items-center': this.align === 'center',
+			'items-end': this.align === 'end',
+			'items-stretch': this.align === 'stretch',
+			'justify-baseline': this.align === 'baseline',
 
-      // Align
-      'justify-center': this.align === 'center',
-      'justify-end': this.align === 'end',
-      'justify-start': this.align === 'start',
-      'justify-stretch': this.align === 'stretch',
-      'justify-baseline': this.align === 'baseline'
-    }
+			// Justify
+			'justify-center': this.justify === 'center',
+			'justify-end': this.justify === 'end',
+			'justify-start': this.justify === 'start',
+			'justify-stretch': this.justify === 'stretch',
+		}
 
-    const styles = {
-      gap: this.gap
-    }
-    return html`
-      <section class=${classMap(classes)} style=${styleMap(styles)}>
-        <slot></slot>
-      </section>
-    `
-  }
+		const styles = {
+			gap: this.gap,
+		}
+		return html`
+			<section class=${classMap(classes)} style=${styleMap(styles)}>
+				<slot></slot>
+			</section>
+		`
+	}
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'schmancy-flex': SchmancyFlex
-  }
+	interface HTMLElementTagNameMap {
+		'schmancy-flex': SchmancyFlex
+	}
 }
