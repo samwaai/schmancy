7f79e8d0daaefac91be3f3e8476940a1b33bd2a1
mhmo91 <eng.mhmo91@gmail.com>
2024-01-19 13:03:05 -0300

    .

--- src/animated-text/animated-text.ts
+++ src/animated-text/animated-text.ts
@@ -23,7 +23,7 @@ import {
 export default class SchmancyAnimatedText extends $LitElement(css`
 	:host {
 		display: block;
-		line-height: 1 !important;
+		line-height: inherit !important;
 	}
 	.ml7 {
 		position: relative;
