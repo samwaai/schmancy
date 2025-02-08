1ae9fb783b93ac2f9c430db0c2ebf9c10393fe66
mhmo91 <eng.mhmo91@gmail.com>
2025-02-08 10:34:42 +0100

    .

--- src/layout/scroll/scroll.ts
+++ src/layout/scroll/scroll.ts
@@ -1,6 +1,6 @@
 import { TailwindElement } from '@mixins/index'
 import { css, html } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { customElement, property } from 'lit/decorators.js'
 
 @customElement('schmancy-scroll')
 export class SchmancyScroll extends TailwindElement(css`
@@ -8,18 +8,45 @@ export class SchmancyScroll extends TailwindElement(css`
 		height: 100%;
 		width: 100%;
 		overflow: hidden;
-		box-shadow: border-box;
+		box-sizing: border-box; /* Ensures proper sizing */
 		display: block;
 		position: relative;
 		inset: 0px;
 	}
+	.scrollbar-hide {
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
+	.scrollbar-hide::-webkit-scrollbar {
+		display: none; /* Chrome, Safari, and Opera */
+	}
 `) {
+	/**
+	 * Determines whether the scrollbar is hidden.
+	 *
+	 * When `hide` is true, the inner scrollable div receives the `scrollbar-hide` class,
+	 * which hides scrollbars in supported browsers.
+	 *
+	 * @attr hide
+	 * @example <schmancy-scroll hide></schmancy-scroll>
+	 */
+	@property({ type: Boolean, reflect: true })
+	public hide = false
+
 	render() {
-		return html` <div class="relative inset-0 h-full w-full overscroll-none">
-			<div class="h-full w-full inset-0 overflow-x-scroll overflow-y-scroll scroll-smooth overscroll-contain">
-				<slot></slot>
+		// The classes are dynamically assigned based on the `hide` property.
+		const classes = {
+			'h-full w-full inset-0 overflow-x-scroll overflow-y-scroll scroll-smooth overscroll-contain': true,
+			'scrollbar-hide': this.hide,
+		}
+
+		return html`
+			<div class="relative inset-0 h-full w-full overscroll-none">
+				<div class=${this.classMap(classes)}>
+					<slot></slot>
+				</div>
 			</div>
-		</div>`
+		`
 	}
 }
 
