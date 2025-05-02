c3114bb7de4408cd71194ead0af9abf19b8ee0d0
mhmo91 <eng.mhmo91@gmail.com>
2025-05-02 19:51:01 +0200

    .

--- src/radio-group/radio-button.ts
+++ src/radio-group/radio-button.ts
@@ -1,75 +1,75 @@
+import { TailwindElement } from '@mixins/index'
 import { html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import { TailwindElement } from '@mixins/index'
 
 @customElement('schmancy-radio-button')
-export class RadioButton extends TailwindElement('') {
-    @property({ type: String }) value = ''
-    @property({ type: Boolean, reflect: true }) checked = false
-    @property({ type: Boolean }) disabled = false
-    @property({ type: String }) name = ''
+export class RadioButton extends TailwindElement() {
+	@property({ type: String }) value = ''
+	@property({ type: Boolean, reflect: true }) checked = false
+	@property({ type: Boolean }) disabled = false
+	@property({ type: String }) name = ''
 
-    connectedCallback() {
-        super.connectedCallback()
-        // Find parent radio-group if exists
-        this.addEventListener('click', this.handleClick)
-    }
+	connectedCallback() {
+		super.connectedCallback()
+		// Find parent radio-group if exists
+		this.addEventListener('click', this.handleClick)
+	}
 
-    disconnectedCallback() {
-        super.disconnectedCallback()
-        this.removeEventListener('click', this.handleClick)
-    }
+	disconnectedCallback() {
+		super.disconnectedCallback()
+		this.removeEventListener('click', this.handleClick)
+	}
 
-    private handleClick() {
-        if (this.disabled) return
-        
-        // Find parent radio-group if exists
-        const radioGroup = this.closest('schmancy-radio-group')
-        if (radioGroup) {
-            // Let the radio-group handle the change
-            const event = new CustomEvent('radio-button-click', {
-                detail: { value: this.value },
-                bubbles: true,
-                composed: true
-            })
-            this.dispatchEvent(event)
-        } else {
-            // Standalone usage
-            this.checked = true
-            this.dispatchEvent(
-                new CustomEvent('change', {
-                    detail: { value: this.value },
-                    bubbles: true,
-                    composed: true
-                })
-            )
-        }
-    }
+	private handleClick() {
+		if (this.disabled) return
 
-    render() {
-        return html`
-            <label class="relative flex items-start cursor-pointer">
-                <div class="flex items-center h-6">
-                    <input
-                        type="radio"
-                        class="h-4 w-4 text-primary focus:ring-primary-light border-gray-300"
-                        .value=${this.value}
-                        .checked=${this.checked}
-                        .disabled=${this.disabled}
-                        .name=${this.name}
-                        @change=${() => {}} /* Let the click handler manage this */
-                    />
-                </div>
-                <div class="ml-3">
-                    <slot name="label"></slot>
-                </div>
-            </label>
-        `
-    }
+		// Find parent radio-group if exists
+		const radioGroup = this.closest('schmancy-radio-group')
+		if (radioGroup) {
+			// Let the radio-group handle the change
+			const event = new CustomEvent('radio-button-click', {
+				detail: { value: this.value },
+				bubbles: true,
+				composed: true,
+			})
+			this.dispatchEvent(event)
+		} else {
+			// Standalone usage
+			this.checked = true
+			this.dispatchEvent(
+				new CustomEvent('change', {
+					detail: { value: this.value },
+					bubbles: true,
+					composed: true,
+				}),
+			)
+		}
+	}
+
+	render() {
+		return html`
+			<label class="relative flex items-start cursor-pointer">
+				<div class="flex items-center h-6">
+					<input
+						type="radio"
+						class="h-4 w-4 text-primary focus:ring-primary-light border-gray-300"
+						.value=${this.value}
+						.checked=${this.checked}
+						.disabled=${this.disabled}
+						.name=${this.name}
+						@change=${() => {}}
+					/>
+				</div>
+				<div class="ml-3">
+					<slot name="label"></slot>
+				</div>
+			</label>
+		`
+	}
 }
 
 declare global {
-    interface HTMLElementTagNameMap {
-        'schmancy-radio-button': RadioButton
-    }
+	interface HTMLElementTagNameMap {
+		'schmancy-radio-button': RadioButton
+	}
 }
