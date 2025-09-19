f7b95b8d1c504ec0cd006628989885a6d8025f27
mhmo91 <eng.mhmo91@gmail.com>
2025-09-19 17:50:43 +0200

    .

--- src/chips/index.ts
+++ src/chips/index.ts
@@ -1,19 +1,5 @@
-// Export SchmancyFilterChip as SchmancyChip for backward compatibility
-export { SchmancyChip } from './filter-chip'
-export type { SchmancyChipChangeEvent } from './filter-chip'
-
-// Export all new chip variants
-export { SchmancyAssistChip } from './assist-chip'
-export type { AssistChipActionEvent } from './assist-chip'
-
-export { SchmancyFilterChip } from './filter-chip'
-export type { FilterChipChangeEvent, FilterChipRemoveEvent } from './filter-chip'
-
-export { SchmancyInputChip } from './input-chip'
-export type { InputChipClickEvent, InputChipRemoveEvent } from './input-chip'
-
-export { SchmancySuggestionChip } from './suggestion-chip'
-export type { SuggestionChipActionEvent } from './suggestion-chip'
-
-// Export the chips container
-export { default as SchmancyChips } from './chips'
+export * from './assist-chip';
+export * from './chips';
+export * from './filter-chip';
+export * from './input-chip';
+export * from './suggestion-chip';
